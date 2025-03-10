435a174a4220683d88fd8d9bb4f9e40d78d91e62
54FiftyFour <63739116+54FiftyFour@users.noreply.github.com>
2025-03-10 20:07:28 +0000

    Hashing but not peppering

---
+++ food blog/app/Hash.js
@@ -0,0 +1,30 @@
+const { createHash, randomBytes } = require('node:crypto');
+
+function hash(string) {
+    return createHash('sha256').update(string);
+}
+function saltAdd (hashString, salt){
+    return hashString.update(salt + hashString);
+}
+function pepperAdd (hashString, pepper){
+    return hashString.update(pepper + hashString);
+}
+function getPepper(){
+    const json = require('./Pepper.json');
+    return json.pepper;
+}
+function createSalt(){
+    return randomBytes(128).toString("hex");
+}
+function hashString(string, salt) {
+    let pepper = getPepper();
+    let hashv1 = saltAdd(string,salt);
+    return hash(pepperAdd(hashv1, pepper));
+}
+function createSecurePassword(){
+    let salt = createSalt();
+    let pepper = getPepper();
+    let hashv1 = saltAdd(string,salt);
+    let password = hash(pepperAdd(hashv1, pepper));
+    return {salt , password};
+}
