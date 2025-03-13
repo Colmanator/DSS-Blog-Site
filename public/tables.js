eac0f931f5c89db5adea22cf4644ebddafe73994
Ethan Colman <colmanator03@gmail.com>
2025-03-13 15:42:20 +0000

    unfinished post table creator

--- public/tables.js
+++ public/tables.js
@@ -10,7 +10,14 @@ class TableDataManager {
 
         await client.connect();
         await client.query("SET SEARCH_PATH TO dss_cw; SET DATESTYLE TO \'ISO, DMY\'"); //Date format set
-        await clienit.query("CREATE TABLE users (column1 email, column2 displayName, column3 password, column4 salt)")
+        await client.query("CREATE TABLE users (column1 email, column2 displayName, column3 password, column4 salt)")
+        await client.end()
+    }
+    async create_post_table(client){
+
+        await client.connect();
+        await client.query("SET SEARCH_PATH TO dss_cw; SET DATESTYLE TO \'ISO, DMY\'"); //Date format set
+        await client.query("CREATE TABLE posts (title , summary, rating, premium ingredients, method)")
         await client.end()
     }
 }
